2ecbf67ebc6ef686a78e64db629e55239b354164
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2022-12-06 21:21:38 +0530

    fixed nill issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   # GET /posts/1 or /posts/1.json
   def show
     @post.update(views: @post.views + 1)
-    @comments = @post.comments
+    @comments = @post.comments.where.not(id: nil)
   end
 
   # GET /posts/new
